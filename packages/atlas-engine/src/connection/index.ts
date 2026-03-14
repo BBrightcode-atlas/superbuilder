979239dad807fb387e9c498a83de197f311087ba
bbright <bbright@bbrightui-iMac.local>
2026-03-15 01:14:31 +0900

    feat(atlas-engine): add connection module barrel export

---
+++ packages/atlas-engine/src/connection/index.ts
@@ -0,0 +1,5 @@
+export { deriveConnections } from "./deriver";
+export { insertAtMarker } from "./applier";
+export { registerWidgetExport } from "./widget-export";
+export { applyConnections } from "./apply-connections";
+export type { DerivedConnections } from "./types";
